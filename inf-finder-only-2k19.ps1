ce4dde49fc16b39fb303afefbf517f31efe5576b
sofckinhateyou <atk29848@gmail.com>
2025-07-21 16:51:43 +1000

    deleted-old-creds

--- inf-finder-only-2k19.ps1
+++ inf-finder-only-2k19.ps1
@@ -1,17 +1,25 @@
-$mountDir = X
-$driverRoot = X
+п»ї$mountWim = "D:\iso\MSWIN\windowsServer_2019\17763.3650.221105-1748.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_ru-ru\sources\boot.wim"
+$mountDir = "D:\mount"
+dism /Mount-Wim /WimFile:"$mountWim" /Index:1 /MountDir:"$mountDir"
 
-# Проверка, существует ли монтированная директория
+$mountDir = "D:\mount"
+$driverRoot = "D:\iso\virtio-win-0.1.266"
+
+# РџСЂРѕРІРµСЂРєР°, СЃСѓС‰РµСЃС‚РІСѓРµС‚ Р»Рё РјРѕРЅС‚РёСЂРѕРІР°РЅРЅР°СЏ РґРёСЂРµРєС‚РѕСЂРёСЏ
 if (-not (Test-Path $mountDir)) {
-    Write-Error "Образ не смонтирован или указан неверный путь: $mountDir"
+    Write-Error "РћР±СЂР°Р· РЅРµ СЃРјРѕРЅС‚РёСЂРѕРІР°РЅ РёР»Рё СѓРєР°Р·Р°РЅ РЅРµРІРµСЂРЅС‹Р№ РїСѓС‚СЊ: $mountDir"
     exit
 }
 
-# Поиск всех .inf файлов по шаблону пути
+# РџРѕРёСЃРє РІСЃРµС… .inf С„Р°Р№Р»РѕРІ РїРѕ С€Р°Р±Р»РѕРЅСѓ РїСѓС‚Рё
 Get-ChildItem -Path $driverRoot -Recurse -Filter "*.inf" | 
     Where-Object { $_.FullName -match [regex]::Escape("\2k19\amd64\") } |
     ForEach-Object {
         $infPath = $_.FullName
-        Write-Host "Добавляю драйвер: $infPath"
+        Write-Host "Р”РѕР±Р°РІР»СЏСЋ РґСЂР°Р№РІРµСЂ: $infPath"
         dism /Image:"$mountDir" /Add-Driver /Driver:"$infPath" /ForceUnsigned
     }
+
+dism /Unmount-Image /MountDir:"$mountDir" /Commit
+
+# .\oscdimg -l"Custom_Server_2019_4" -t -u1 -b"D:\iso\MSWIN\windowsServer_2019\17763.3650.221105-1748.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_ru-ru\boot\etfsboot.com" "D:\iso\MSWIN\windowsServer_2019\17763.3650.221105-1748.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_ru-ru" "D:\iso\Custom_Server_maxedit_2019.iso"
